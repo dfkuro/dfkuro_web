fb935932a51b803e12edc77426df506e7b7bcd73
dfkuro <izmirreffi@gmail.com>
2017-04-12 00:07:39 -0500

    React Clock
    First attempt with React and codeigniter working.

---
+++ application/views/pages/home_view.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello React</title>
+  </head>
+  <body>
+    <h3>welcome back dfkuro.</h3>
+    <br>
+    <div id="hello"></div>
+    <div id="world"></div>
+
+    <div id="clock"></div>
+
+    <script type="text/javascript" src="<?=base_url('src/build/bundle.js')?>"></script>
+  </body>
+</html>
---
+++ src/dev/main.js
@@ -0,0 +1,3 @@
+//import Hello from './jsx/hello.jsx';
+//import World from './jsx/world.jsx';
+import Clock from './jsx/clock.jsx';
